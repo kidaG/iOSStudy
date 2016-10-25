0e056052d3b735d76cb63ca0ce6d89f4590bec42
Jirokichi <topaz30th.kdy@gmail.com>
2016-10-25 20:27:19 +0900

    UICollectionViewでxibを使うための検証用Projects追加

---
+++ SampleMacApplication/SampleMacApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleMacApplication
+//
+//  Created by yuya on 2016/02/03.
+//  Copyright © 2016年 yuya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
